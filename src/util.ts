ee41c040d869c37a501e0411b3891c868274cad3
maytees <mateespublicprofile@gmail.com>
2024-10-03 20:16:22 -0400

    feat: display error when using wrong code

--- src/util.ts
+++ src/util.ts
@@ -51,3 +51,71 @@ export function addTextHover(
     button.color = baseColor;
   });
 }
+
+export function addTitleText(k: KAPLAYCtx) {
+  k.add([
+    k.text("CURSOR", {
+      size: 56,
+      font: "press2p",
+    }),
+    k.anchor("center"),
+    k.color(k.Color.BLACK),
+    k.pos(k.center().x, k.center().y - 100),
+  ]);
+}
+
+// goto is the scene to go back to
+export function addBackButton(
+  k: KAPLAYCtx,
+  goto: string,
+  yOffset: number,
+  text?: string,
+  onClickCallback?: any
+) {
+  const backButton = k.add([
+    k.rect(56 * 6, 16 * 4),
+    k.pos(k.center().x, k.center().y + yOffset),
+    k.color(k.Color.BLACK),
+    k.anchor("center"),
+    k.outline(2, k.Color.BLACK),
+    k.area(),
+    "backButton",
+  ]);
+
+  const backButtonText = backButton.add([
+    k.text(text ? text : "Back", { size: 24, font: "press2p" }),
+    k.anchor("center"),
+    k.color(k.Color.WHITE),
+  ]);
+
+  addTextHover(
+    k,
+    backButton,
+    "backButton",
+    k.Color.WHITE,
+    k.Color.BLACK,
+    backButtonText,
+    true
+  );
+
+  k.onClick("backButton", () => {
+    if (onClickCallback) {
+      onClickCallback();
+    }
+    k.go(goto);
+  });
+}
+
+export function displayError(k: KAPLAYCtx, error: string, lifeSpan?: number) {
+  k.add([
+    k.text(error, {
+      size: 24,
+      font: "press2p",
+    }),
+    k.pos(k.center().x, k.center().y - 340),
+    k.color(k.Color.RED),
+    k.anchor("center"),
+    k.lifespan(lifeSpan ? lifeSpan : 2),
+    k.opacity(1),
+  ]);
+}
